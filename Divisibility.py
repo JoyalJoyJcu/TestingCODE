3bda8a8b3bc1a0ab74f0cc8b20bc54ea53c264d3
joyal <joyal.joy@my.jcu.edu.au>
2025-01-27 23:39:30 +1000

    testing vs code with python, made a yes and no if the new_numb > 0

---
+++ Divisibility.py
@@ -0,0 +1,7 @@
+frist_num = input("whats the frist number: ")
+second_num = input("whats the second number: ")
+new_numb = int(frist_num) / int(second_num)
+if new_numb > 0:
+    print("yes")
+else:
+    print("no")
