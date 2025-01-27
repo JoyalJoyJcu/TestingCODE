ad79b0e2a9720d7034243bde1beefbde79c9de86
joyal <joyal.joy@my.jcu.edu.au>
2025-01-27 23:47:51 +1000

    fixed divisibilitiy file and made sure it worked

--- Pyton/Divisibility.py
+++ Pyton/Divisibility.py
@@ -1,8 +1,8 @@
 frist_num = input("whats the frist number: ")
 second_num = input("whats the second number: ")
 new_numb = int(frist_num) / int(second_num)
-if new_numb > 0:
-    print("yes")
+if new_numb > 1:
+    print("yes the number is divisable")
 else:
-    print("no")
+    print("no the number isn't divisable")
     
