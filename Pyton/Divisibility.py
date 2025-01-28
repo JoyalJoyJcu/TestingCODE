0f0fc40d3f6ce083dba7703d0cd72f0294f9e76a
joyal <joyal.joy@my.jcu.edu.au>
2025-01-28 15:42:20 +1000

    simple error checking

--- Pyton/Divisibility.py
+++ Pyton/Divisibility.py
@@ -1,8 +1,20 @@
-frist_num = input("whats the frist number: ")
-second_num = input("whats the second number: ")
-new_numb = int(frist_num) / int(second_num)
-if new_numb > 1:
-    print("yes the number is divisable")
-else:
-    print("no the number isn't divisable")
+def main():
+    """this funtion will test if two number are divisable or not and print a responce """
+    frist_num = vaild_number()
+    second_num = vaild_number()
+    new_numb = int(frist_num) / int(second_num)
+    if new_numb > 1:
+        print("yes the number is divisable")
+    else:
+        print("no the number isn't divisable")
+
+def vaild_number():
+    number = input("whats the number: ")
+    while int(number) > 0:
+        return number
+    number = input("whats the number: ")
+
     
+
+
+main()
